8a1d06b48a88154f14c73d3de6980f46d7bb4a98
Tobías Demoor <53862471+TobiasDemoor@users.noreply.github.com>
2021-07-26 12:30:52 -0300

    leetcode 108

---
+++ leet.108.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+        
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        # define root
+        length = len(nums)
+        if length == 0:
+            return None
+        middle = length//2
+        root = TreeNode(nums[middle])
+        root.left = self.sortedArrayToBST(nums[:middle])
+        root.right = self.sortedArrayToBST(nums[middle+1:])
+        
+        return root
+
+sol = Solution()
+print(sol.sortedArrayToBST([-10,-3,0,5,9]))
