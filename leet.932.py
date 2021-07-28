1d54a930a28e2d2192ea1259450a4274851e1174
TobiasDemoor <tobias.demoor@hotmail.com>
2021-07-28 17:07:14 -0300

    LeetCode 932

---
+++ leet.932.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def beautifulArray(self, n):
+        """
+        :type n: int
+        :rtype: List[int]
+        """
+        ans = [1]
+        while len(ans) < n:
+            tmp1 = []
+            tmp2 = []
+            for i in ans:
+                aux = i*2-1
+                if aux <= n:
+                    tmp1.append(aux)
+                aux = i*2
+                if aux <= n:
+                    tmp2.append(aux)
+            ans = tmp1 + tmp2
+        return ans
+
+sol = Solution()
+print(sol.beautifulArray(4))
