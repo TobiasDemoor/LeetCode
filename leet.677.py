1af5faaeb595d57c5fc762f47c471b9c265d997e
TobiasDemoor <tobias.demoor@hotmail.com>
2021-07-30 17:12:43 -0300

    attempt alternative solution

--- leet.677.py
+++ leet.677.py
@@ -1,3 +1,4 @@
+# Solution 1: works without any issue uses a lot of memory
 class MapSum(object):
 
     def __init__(self):
@@ -25,6 +26,60 @@ class MapSum(object):
         for key in filter(lambda e : e.startswith(prefix) ,self.map.keys()):
             sum +=self.map[key]
         return sum
+
+
+# Solution 2: a tree (does not work beacause you should overwrite key value pair)
+# class TreeNode(object):
+#     def __init__(self, char, val):
+#         self.char = char
+#         self.val = val
+#         self.children = {}
+
+# class MapSum(object):
+
+#     def __init__(self):
+#         """
+#         Initialize your data structure here.
+#         """
+#         self.root = TreeNode("", 0)
+        
+
+#     def insert(self, key, val):
+#         """
+#         :type key: str
+#         :type val: int
+#         :rtype: None
+#         """
+#         aux = self.root
+        
+#         i = 0
+#         while i < len(key):
+#             c = key[i]
+#             aux.val += val
+#             if c in aux.children:
+#                 aux = aux.children[c]
+#                 i += 1
+#             else:
+#                 break
+
+#         for c in key[i:]:
+#             child = TreeNode(c, val)
+#             aux.children[c] = child
+#             aux = child
+        
+
+#     def sum(self, prefix):
+#         """
+#         :type prefix: str
+#         :rtype: int
+#         """
+#         aux = self.root
+#         for c in prefix:
+#             if c in aux.children:
+#                 aux = aux.children[c]
+#             else:
+#                 return 0
+#         return aux.val
         
 
 
