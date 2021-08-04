7beff63685d78c24d59bec79be96fcc84b114dad
TobiasDemoor <tobias.demoor@hotmail.com>
2021-08-04 11:02:15 -0300

    LeetCode 113

---
+++ leet.113.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def recursive(self, node, acum, path, target, result):
+        if node != None:
+            acum += node.val
+            newPath = path + [node.val]
+            if acum == target and node.left == None and node.right == None:
+                result.append(newPath)
+            else:
+                self.recursive(node.left, acum, newPath, target, result)
+                self.recursive(node.right, acum, newPath, target, result)
+
+    def pathSum(self, root, targetSum):
+        """
+        :type root: TreeNode
+        :type targetSum: int
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.recursive(root, 0, [], targetSum, result)
+        return result
