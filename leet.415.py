3cf50c5285941fe5555ec0457a5359725f0481f0
TobiasDemoor <tobias.demoor@hotmail.com>
2021-08-09 12:52:41 -0300

    LeetCode 415

---
+++ leet.415.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        hashMap = {}
+        for i in range(10):
+            for j in range(i, 10):
+                hashMap[(str(i), str(j))] = i+j
+
+        m = len(num1)
+        n = len(num2)
+        result = ""
+        if m < n:
+            num1 = "0" * (n-m) + num1
+        else:
+            num2 = "0" * (m-n) + num2
+        i = max(n, m) - 1
+        carry = False
+        while i >= 0:
+            aux = tuple(sorted((num1[i], num2[i])))
+            val = hashMap[aux] + (1 if carry else 0)
+            if val > 9:
+                carry = True
+                val %= 10
+            else:
+                carry = False
+            result = str(val) + result
+            i -= 1
+        if carry:
+            result = "1" + result
+        return result
+
+
+sol = Solution()
+print(sol.addStrings("11", "123"))
+print(sol.addStrings("456", "77"))
