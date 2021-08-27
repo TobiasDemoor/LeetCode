dc0861c4edfd8338500316aeb75e80686a0fcad5
TobiasDemoor <tobias.demoor@hotmail.com>
2021-08-27 09:24:55 -0300

    LeetCode 522 improvements

--- leet.522.py
+++ leet.522.py
@@ -4,35 +4,27 @@ import re
 
 class Solution:
     def findLUSlength(self, strs: List[str]) -> int:
-        def genRegexPattern(s:str) -> str:
-            return ".*"+".*".join(s)+".*"
         strs.sort(key=len)
-        dp = [[strs[0]]]
+        last = [strs[0]]
         for i in range(1, len(strs)):
             string = strs[i]
             lusList = []
-            for s in dp[i-1]:
-                if not bool(re.match(genRegexPattern(s), string)):
+            for s in last:
+                if not bool(re.match(".*"+".*".join(s)+".*", string)):
                     lusList.append(s)
             j = i-1
             l = len(string)
-            regex = genRegexPattern(string)
-            while j > -1 and len(strs[j]) == l and not bool(re.match(regex, strs[j])):
+            while j > -1 and len(strs[j]) == l and not strs[j] == string:
                 j -= 1
             if j == -1 or len(strs[j]) < l:
                 lusList.append(string)
-            dp.append(lusList)
-        
-        print(dp)
-        if len(dp[-1]) == 0:
+            last = lusList
+
+        if len(last) == 0:
             return -1
-        
-        return len(dp[-1][-1])
-        
-            
+
+        return len(last[-1])
+
 
 sol = Solution()
 sol.findLUSlength(["abcd", "axxbxc"])
-            
-
-                
