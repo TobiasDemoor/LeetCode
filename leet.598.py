996397a812f4dc4a2a7e42a48d3bdb7edc515be6
TobiasDemoor <tobias.demoor@hotmail.com>
2021-08-30 08:13:50 -0300

    LeetCode 598

---
+++ leet.598.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def maxCount(self, m: int, n: int, ops: List[List[int]]) -> int:
+        minM = m
+        minN = n
+        for op in ops:
+            minM = min(minM, op[0])
+            minN = min(minN, op[1])
+        return minM*minN
